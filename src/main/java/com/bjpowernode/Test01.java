1a254c3e366b12dd066092c61a8f59abb9411c5b
guoxinyu <guoxinyu@163.com>
2020-12-07 20:38:45 +0800

    二次提交

---
+++ src/main/java/com/bjpowernode/Test01.java
@@ -0,0 +1,9 @@
+package com.bjpowernode;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaa");
+    }
+}
